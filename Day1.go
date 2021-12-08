2791df6e08aa3c9030dea8277c3f369cbaae776d
Hassam <Hassam.styleshop@gmail.com>
2021-12-08 12:59:19 +0500

    Day1 - completed

---
+++ Day1.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var total, previous int
+	total = 0
+	previous = 0
+	file, err := os.Open("Inputs.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	for scanner.Scan() {
+		current, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			// handle error
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		if current > previous {
+			total += 1
+		}
+		previous = current
+	}
+	fmt.Println(total)
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
